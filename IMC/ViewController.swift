1bc435888610da1abaf33076a16517b1d4093c6c
Viviane Verbeno <vikaverbeno@gmail.com>
2021-12-30 14:53:05 -0300

    Primeiro Commit

--- IMC/ViewController.swift
+++ IMC/ViewController.swift
@@ -9,7 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var tf_peso: UITextField!
+    @IBOutlet weak var tf_altura: UITextField!
+    @IBOutlet weak var lb_resultado: UILabel!
+    @IBOutlet weak var im_resultado: UIImageView!
+    
+    var imc: Double = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +28,18 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func calcular(_ sender: Any) {
+        if let weight = Double(tf_peso.text!), let height = Double(tf_altura.text!) {
+            imc = weight / (height*height)
+            showResolts()
+            
+        }
+    }
+    
+    func showResolts(){
+        var resolt: String = ""
+        var image: String = ""
+        
+    }
 }
 
